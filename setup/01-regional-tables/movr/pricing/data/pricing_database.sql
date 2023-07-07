1168ef4b02663e853795a23b2d8086de78362963
kikiya <kikia@cockroachlabs.com>
2023-07-07 10:26:37 -0400

    pricing primary region should be us-east

--- setup/01-regional-tables/movr/pricing/data/pricing_database.sql
+++ setup/01-regional-tables/movr/pricing/data/pricing_database.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE movr_pricing;
 
-ALTER DATABASE movr_pricing SET PRIMARY REGION "us-west";
+ALTER DATABASE movr_pricing SET PRIMARY REGION "us-east";
 ALTER DATABASE movr_pricing ADD REGION "us-central";
-ALTER DATABASE movr_pricing ADD REGION "us-east";
+ALTER DATABASE movr_pricing ADD REGION "us-west";
 
 ALTER DATABASE movr_pricing SURVIVE REGION FAILURE;
