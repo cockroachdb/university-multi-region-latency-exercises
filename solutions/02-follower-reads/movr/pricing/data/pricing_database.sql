0e5ce230ed9ae7737c6c6ae869ed1d7b3522904c
kikiya <kikia@cockroachlabs.com>
2023-07-07 10:50:58 -0400

    add region survival to pricing setup in solutiosn 2 & 4

--- solutions/02-follower-reads/movr/pricing/data/pricing_database.sql
+++ solutions/02-follower-reads/movr/pricing/data/pricing_database.sql
@@ -4,6 +4,8 @@ ALTER DATABASE movr_pricing SET PRIMARY REGION "us-east";
 ALTER DATABASE movr_pricing ADD REGION "us-central";
 ALTER DATABASE movr_pricing ADD REGION "us-west";
 
+ALTER DATABASE movr_pricing SURVIVE REGION FAILURE;
+
 CREATE TABLE movr_pricing.vip_rates (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     rate_code STRING NOT NULL,
